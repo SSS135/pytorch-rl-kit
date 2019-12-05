ae41a938b091e65dfd9f8fab42b65e93cbf1bdd5
Alexander Penkin <sss13594@gmail.com>
2019-12-05 14:56:42 +0500

    update MuZero

--- ppo_pytorch/algs/mu_zero.py
+++ ppo_pytorch/algs/mu_zero.py
@@ -1,71 +1,101 @@
-from asyncio import Future
-from concurrent.futures import ThreadPoolExecutor
-from copy import deepcopy
 from functools import partial
-from typing import Optional
 
-import gym.spaces
 import torch
 import torch.autograd
 import torch.autograd
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.nn.utils import clip_grad_norm_
+from ppo_pytorch.actors.cat_value_encoder import CategoricalValueEncoder
 from torchvision.utils import make_grid
 
 from .replay_buffer import ReplayBuffer
-from .utils import blend_models
-from ..actors import ModularActor, create_td3_fc_actor
-from ..actors.utils import model_diff
+from ..actors.utils import model_diff, normalized_columns_initializer_
 from ..common.attr_dict import AttrDict
-from ..common.barron_loss import barron_loss
-from ..common.data_loader import DataLoader
 from ..common.probability_distributions import DiagGaussianPd, CategoricalPd, make_pd
 from ..common.rl_base import RLBase
 import torch.nn as nn
 from ..common.gae import calc_value_targets
+import torch.jit
 
 
-N_H = 128
+N_H = 256
+
+
+# @torch.jit.script
+# def min_max_norm(x):
+#     min, max = x.min(-1, keepdim=True)[0], x.max(-1, keepdim=True)[0]
+#     return (x - min) / (max - min)
+#
+#
+# class MinMaxNorm(nn.Module):
+#     def forward(self, x):
+#         return min_max_norm(x)
 
 
 class MuZeroPredictionNet(nn.Module):
-    def __init__(self, obs_len, action_len, logits_len):
+    def __init__(self, obs_len: int, action_len: int, logits_len: int,
+                 reward_bins: int = 15, value_bins: int = 25, reward_max: float = 15, value_max: float = 300):
         super().__init__()
         self.obs_len = obs_len
         self.action_len = action_len
         self.logits_len = logits_len
-        self.start_norm = nn.LayerNorm(N_H)
+        self.reward_bins = reward_bins
+        self.value_bins = value_bins
+        self.reward_max = reward_max
+        self.value_max = value_max
+        self.reward_encoder = CategoricalValueEncoder(reward_max, reward_bins)
+        self.value_encoder = CategoricalValueEncoder(value_max, value_bins)
+        # self.start_norm = nn.LayerNorm(N_H)
         self.action_encoder = nn.Linear(action_len, N_H)
-        self.gate = nn.Sequential(nn.Linear(N_H, N_H), nn.Sigmoid())
-        self.reward_value_logits_encoder = nn.Linear(N_H, 2 + logits_len)
+        # self.gate = nn.Sequential(nn.LayerNorm(N_H), nn.LeakyReLU(0.1), nn.Linear(N_H, N_H), nn.Sigmoid())
+        self.reward_output = nn.Sequential(
+            nn.Linear(N_H, N_H),
+            nn.ReLU(),
+            nn.Linear(N_H, reward_bins)
+        )
+        self.value_logits_output = nn.Sequential(
+            nn.Linear(N_H, N_H),
+            nn.ReLU(),
+            nn.Linear(N_H, value_bins + logits_len)
+        )
         self.next_state_model = nn.Sequential(
-            # nn.LayerNorm(N_H),
-            nn.LeakyReLU(0.1),
+            nn.ReLU(),
             nn.Linear(N_H, N_H),
-            nn.LeakyReLU(0.1),
-            nn.Linear(N_H, N_H),
+            nn.ReLU(),
+            nn.Linear(N_H, N_H * 2),
         )
-        self.observation_encoder = nn.Sequential(
+        self.observation_model = nn.Sequential(
             nn.Linear(obs_len, N_H),
-            nn.LeakyReLU(0.1),
+            nn.ReLU(),
             nn.Linear(N_H, N_H),
-            # nn.LayerNorm(N_H),
+            nn.ReLU(),
+            nn.Linear(N_H, N_H),
+            # MinMaxNorm(),
         )
+
+        normalized_columns_initializer_(self.reward_output[-1].weight.data, 0.01)
+        self.reward_output[-1].bias.data.fill_(0)
+        normalized_columns_initializer_(self.value_logits_output[-1].weight.data, 0.01)
+        self.value_logits_output[-1].bias.data.fill_(0)
 
     def forward(self, hidden, actions):
-        x = self.start_norm(hidden) + self.action_encoder(actions)
-        x = self.next_state_model(x)
-        hidden = hidden + self.gate(x) * x
+        x = hidden + self.action_encoder(actions)
+        x, gate = self.next_state_model(x).chunk(2, -1)
+        gate = gate.sigmoid()
+        hidden = gate * hidden + (1 - gate) * x
+        hidden = F.layer_norm(hidden, [hidden.shape[-1]])
         return self._get_output(hidden)
 
+    @torch.jit.export
     def encode_observation(self, observation):
-        hidden = self.observation_encoder(observation)
+        hidden = self.observation_model(observation)
+        hidden = F.layer_norm(hidden, [hidden.shape[-1]])
         return self._get_output(hidden)
 
     def _get_output(self, hidden):
-        reward, value, logits = self.reward_value_logits_encoder(hidden).split_with_sizes([1, 1, self.logits_len], -1)
-        return AttrDict(hidden=hidden, rewards=reward.squeeze(-1), state_values=value.squeeze(-1), logits=logits)
+        reward = self.reward_output(hidden)
+        value, logits = self.value_logits_output(hidden).split_with_sizes([self.value_bins, self.logits_len], -1)
+        return {'hidden': hidden, 'reward_bins': reward, 'state_value_bins': value, 'logits': logits}
 
 
 class MuZero(RLBase):
@@ -73,7 +103,6 @@ class MuZero(RLBase):
                  reward_discount=0.99,
                  horizon=16,
                  batch_size=32,
-                 num_batches=4,
                  num_actions=4,
                  num_rollouts=16,
                  rollout_depth=5,
@@ -83,7 +112,7 @@ class MuZero(RLBase):
                  cuda_eval=True,
                  cuda_train=True,
                  grad_clip_norm=None,
-                 reward_scale=1.0,
+                 # reward_scale=1.0,
                  barron_alpha_c=(1.5, 1),
                  lr_scheduler_factory=None,
                  entropy_decay_factory=None,
@@ -93,28 +122,26 @@ class MuZero(RLBase):
         self.reward_discount = reward_discount
         self.horizon = horizon
         self.batch_size = batch_size
-        self.num_batches = num_batches
         self.replay_buffer_size = replay_buffer_size
         self.target_model_blend = target_model_blend
         self.device_eval = torch.device('cuda' if cuda_eval else 'cpu')
         self.device_train = torch.device('cuda' if cuda_train else 'cpu')
         self.grad_clip_norm = grad_clip_norm
-        self.reward_scale = reward_scale
+        # self.reward_scale = reward_scale
         self.barron_alpha_c = barron_alpha_c
         self.num_rollouts = num_rollouts
         self.rollout_depth = rollout_depth
         self.num_actions = num_actions
 
         self.pd = make_pd(action_space)
-        self._train_model: MuZeroPredictionNet = MuZeroPredictionNet(
-            observation_space.shape[0], self.pd.input_vector_len, self.pd.prob_vector_len)
+        self._train_model: MuZeroPredictionNet = torch.jit.script(MuZeroPredictionNet(
+            observation_space.shape[0], self.pd.input_vector_len, self.pd.prob_vector_len))
 
         if self.model_init_path is not None:
             self._train_model.load_state_dict(torch.load(self.model_init_path))
             print(f'loaded model {self.model_init_path}')
 
         self._train_model = self._train_model.to(self.device_train)
-        self._eval_model = deepcopy(self._train_model).to(self.device_eval)
 
         self._optimizer: torch.optim.Optimizer = optimizer_factory(self._train_model.parameters())
         self._replay_buffer = ReplayBuffer(replay_buffer_size)
@@ -172,27 +199,43 @@ class MuZero(RLBase):
         # (steps, actors, *)
         data = self._replay_buffer.get_last_samples(self.horizon)
         data = AttrDict(data)
-        data.rewards = self.reward_scale * data.rewards
+        # data.rewards = self.reward_scale * data.rewards
         return data
 
     def _muzero_update(self, data: AttrDict):
-        num_samples = data.states.shape[1]
-
-        state_values_p1 = torch.cat([data.state_values, self._prev_data['state_values'].unsqueeze(0)], 0)
+        state_values_p1 = torch.cat([data.state_values, self._prev_data['state_values'].cpu().unsqueeze(0)], 0)
         data.value_targets = calc_value_targets(data.rewards, state_values_p1, data.dones, self.reward_discount, 0.99)
 
-        rand_idx = torch.randperm(num_samples, device=self.device_train).chunk(self.num_batches)
+        for name, t in data.items():
+            H, B = t.shape[:2]
+            # (B, X, H, 1)
+            image_like = t.reshape(H, B, -1).permute(1, 2, 0).unsqueeze(-1)
+            X = image_like.shape[1]
+            # (B, X * rollout_depth, N)
+            image_like = F.unfold(image_like.float(), (self.rollout_depth, 1)).type_as(image_like)
+            N = image_like.shape[-1]
+            # (B, X, depth, N)
+            image_like = image_like.reshape(B, X, self.rollout_depth, image_like.shape[-1])
+            # (depth, B * N, *)
+            data[name] = image_like.permute(2, 0, 3, 1).reshape(self.rollout_depth, B * N, *t.shape[2:])
 
-        old_model = deepcopy(self._train_model)
+        num_samples = data.states.shape[1]
 
-        with DataLoader(data, rand_idx, self.device_train, 4, dim=1) as data_loader:
-            for batch_index in range(self.num_batches):
-                # prepare batch data
-                batch = AttrDict(data_loader.get_next_batch())
-                loss = self._update_traj(batch, self._do_log and batch_index == self.num_batches - 1)
-                # if self._do_actor_update:
-                #     blend_models(self._train_model, self._target_model, self.target_model_blend)
-                # self._update_iter += 1
+        # rand_idx = torch.randperm(num_samples, device=self.device_train).chunk(self.num_batches)
+
+        old_model = {k: v.clone() for k, v in self._train_model.state_dict().items()}
+
+        # with DataLoader(data, rand_idx, self.device_train, 4, dim=1) as data_loader:
+        #     for batch_index in range(self.num_batches):
+        # prepare batch data
+        # (H, B, *)
+        # batch = AttrDict(data_loader.get_next_batch())
+        # loss = self._update_traj(batch, self._do_log and batch_index == self.num_batches - 1)
+        # if self._do_actor_update:
+        #     blend_models(self._train_model, self._target_model, self.target_model_blend)
+        # self._update_iter += 1
+
+        loss = self._update_traj(AttrDict({k: v.to(self.device_train, non_blocking=True) for k, v in data.items()}), self._do_log)
 
         if self._do_log:
             self.logger.add_scalar('learning rate', self._optimizer.param_groups[0]['lr'], self.frame)
@@ -200,7 +243,7 @@ class MuZero(RLBase):
             self.logger.add_scalar('model abs diff', model_diff(old_model, self._train_model), self.frame)
             self.logger.add_scalar('model max diff', model_diff(old_model, self._train_model, True), self.frame)
 
-        self._eval_model = deepcopy(self._train_model).to(self.device_eval).eval()
+        # self._eval_model = deepcopy(self._train_model).to(self.device_eval).eval()
 
     # @property
     # def _do_actor_update(self):
@@ -212,40 +255,50 @@ class MuZero(RLBase):
                batch.dones.shape == batch.rewards.shape == batch.actions.shape[:-1]
 
         with torch.enable_grad():
-            ac_out = self._train_model.encode_observation(batch.states[0])
-            hidden = ac_out.hidden
+            ac_out = AttrDict(self._train_model.encode_observation(batch.states[0]))
 
             nonterminals = 1 - batch.dones
-            logits = batch.logits[0]
+            vtarg = batch.value_targets[0]
+            logits_target = batch.logits[0]
+            ac_out_prev = ac_out
             cum_nonterm = torch.ones_like(batch.dones[0])
-            losses = 0
+            loss = 0
             for i in range(batch.states.shape[0]):
                 vtarg = (batch.value_targets[i] * cum_nonterm).detach()
-                logits = torch.lerp(logits, batch.logits[i], cum_nonterm).detach()
-                losses += F.mse_loss(ac_out.state_values, vtarg)
-                losses += F.mse_loss(ac_out.logits, logits) - self.pd.logp(batch.actions[i], logits).mean()
+                # vtarg = torch.lerp(vtarg, batch.value_targets[i], cum_nonterm).detach()
+                logits_target = torch.lerp(logits_target, batch.logits[i], cum_nonterm.unsqueeze(-1)).detach()
+                loss += -self._train_model.value_encoder.logp(ac_out.state_value_bins, vtarg).mean()
+                # loss += 0.5 * (logits_target - ac_out.logits).pow(2).mean(-1).mul(cum_nonterm).mean()
+                loss += 5 * self.pd.kl(logits_target, ac_out.logits).sum(-1).mul(cum_nonterm).mean()
+                loss += 0.01 * -self.pd.entropy(ac_out.logits).sum(-1).mul(cum_nonterm).mean()
+                if self.frame > 10000:
+                    loss += -self.pd.logp(batch.actions[i], ac_out.logits).sum(-1).mul(cum_nonterm).mean()
 
                 input_actions = self.pd.to_inputs(batch.actions[i])
-                ac_out = self._train_model(hidden, input_actions)
+                ac_out_prev = ac_out
+                ac_out = AttrDict(self._train_model(ac_out.hidden, input_actions))
                 rtarg = (batch.rewards[i] * cum_nonterm).detach()
-                cum_nonterm *= nonterminals[i]
-                losses += F.mse_loss(ac_out.rewards, rtarg)
+                loss += -self._train_model.reward_encoder.logp(ac_out.reward_bins, rtarg).mean()
+                cum_nonterm = cum_nonterm * nonterminals[i]
 
-            loss = losses.div(batch.states.shape[0])
+            loss = loss / batch.states.shape[0]
 
         if do_log:
-            self.logger.add_scalar('reward rmse', (ac_out.rewards - rtarg).pow(2).mean().sqrt(), self.frame)
-            self.logger.add_scalar('state_values rmse', (ac_out.state_values - vtarg).pow(2).mean().sqrt(), self.frame)
-            self.logger.add_scalar('logits rmse', (ac_out.logits - logits).pow(2).mean().sqrt(), self.frame)
+            rewards = self._train_model.reward_encoder(ac_out.reward_bins)
+            self.logger.add_scalar('reward rmse', (rewards - rtarg).pow(2).mean().sqrt(), self.frame)
+            state_values = self._train_model.value_encoder(ac_out_prev.state_value_bins)
+            self.logger.add_scalar('state_values rmse', (state_values - vtarg).pow(2).mean().sqrt(), self.frame)
+            self.logger.add_scalar('logits rmse', (ac_out_prev.logits - logits_target).pow(2).mean().sqrt(), self.frame)
 
         loss.backward()
+        # clip_grad_norm_(self._train_model.parameters(), 4)
         self._optimizer.step()
         self._optimizer.zero_grad()
 
         return loss
 
     def _search_action(self, states):
-        start_ac_out = self._train_model.encode_observation(states)
+        start_ac_out = AttrDict(self._train_model.encode_observation(states))
         # (B, A, R, X)
         hidden = start_ac_out.hidden\
             .unsqueeze(-2).repeat_interleave(self.num_actions, -2)\
@@ -260,12 +313,14 @@ class MuZero(RLBase):
         value_targets = 0
         for i in range(self.rollout_depth):
             input_actions = self.pd.to_inputs(actions)
-            ac_out = self._train_model(hidden, input_actions)
+            ac_out = AttrDict(self._train_model(hidden, input_actions))
             hidden = ac_out.hidden
             actions = self.pd.sample(ac_out.logits)
-            value_targets = value_targets + self.reward_discount ** i * ac_out.rewards
+            rewards = self._train_model.reward_encoder(ac_out.reward_bins)
+            value_targets += self.reward_discount ** i * rewards
 
-        value_targets += self.reward_discount ** (self.rollout_depth - 1) * ac_out.state_values
+        last_state_values = self._train_model.value_encoder(ac_out.state_value_bins)
+        value_targets += self.reward_discount ** self.rollout_depth * last_state_values
         value_targets = value_targets.topk(self.num_rollouts // 4, -1)[0]
         # (B, A)
         value_targets = value_targets.mean(-1)
@@ -273,7 +328,8 @@ class MuZero(RLBase):
         ac_idx = value_targets.argmax(-1, keepdim=True)
         actions = start_actions.gather(-2, ac_idx.unsqueeze(-1)).squeeze(-2)
         assert actions.shape[:-1] == hidden.shape[:1]
-        return start_ac_out.logits, actions, start_ac_out.state_values
+        start_state_values = self._train_model.value_encoder(start_ac_out.state_value_bins)
+        return start_ac_out.logits, actions, start_state_values
 
     def drop_collected_steps(self):
         self._prev_data = None
