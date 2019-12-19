53ac0012725d0bb6cbc8034ce30786749b287903
Alexander Penkin <sss13594@gmail.com>
2019-12-19 10:57:32 +0500

    add max logit norm to CategoricalPd

--- ppo_pytorch/common/probability_distributions.py
+++ ppo_pytorch/common/probability_distributions.py
@@ -100,9 +100,10 @@ class ProbabilityDistribution:
 
 
 class CategoricalPd(ProbabilityDistribution):
-    def __init__(self, n):
+    def __init__(self, n, max_logit_norm=None):
         super().__init__(locals())
         self.n = n
+        self.max_logit_norm = max_logit_norm
 
     @property
     def prob_vector_len(self):
@@ -120,22 +121,27 @@ class CategoricalPd(ProbabilityDistribution):
     def dtype(self):
         return torch.int64
 
-    def logp(self, a, prob):
-        logp = F.log_softmax(prob, dim=-1)
+    def logp(self, a, logits):
+        logits = self._clamp_logits(logits)
+        logp = F.log_softmax(logits, dim=-1)
         return logp.gather(dim=-1, index=a)
 
-    def kl(self, prob0, prob1):
-        logp0 = F.log_softmax(prob0, dim=-1)
-        logp1 = F.log_softmax(prob1, dim=-1)
+    def kl(self, logits0, logits1):
+        logits0 = self._clamp_logits(logits0)
+        logits1 = self._clamp_logits(logits1)
+        logp0 = F.log_softmax(logits0, dim=-1)
+        logp1 = F.log_softmax(logits1, dim=-1)
         return (logp0.exp() * (logp0 - logp1)).sum(dim=-1, keepdim=True)
 
     def entropy(self, logits):
+        logits = self._clamp_logits(logits)
         logits = logits - logits.logsumexp(dim=-1, keepdim=True)
         p_log_p = logits * logits.softmax(-1)
         return -p_log_p.sum(-1, keepdim=True)
 
-    def sample(self, prob):
-        return F.softmax(prob.reshape(-1, prob.shape[-1]), dim=-1).multinomial(1).reshape(*prob.shape[:-1], -1)
+    def sample(self, logits):
+        logits = self._clamp_logits(logits)
+        return F.softmax(logits.reshape(-1, logits.shape[-1]), dim=-1).multinomial(1).reshape(*logits.shape[:-1], -1)
 
     def to_inputs(self, action):
         with torch.no_grad():
@@ -143,6 +149,11 @@ class CategoricalPd(ProbabilityDistribution):
             onehot.scatter_(dim=-1, index=action, value=1)
             # onehot = onehot - 1 / self.n
         return onehot
+
+    def _clamp_logits(self, logits):
+        if self.max_logit_norm is None:
+            return logits
+        return logits * (self.max_logit_norm / logits.norm(2, dim=-1, keepdim=True).clamp_min(self.max_logit_norm))
 
 
 class MultiCategoricalPd(ProbabilityDistribution):
