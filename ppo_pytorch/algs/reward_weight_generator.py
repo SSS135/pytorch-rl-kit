65cc425990b4e8b35ed9f4858f36c8a0cb10fb2d
Alexander Penkin <sss13594@gmail.com>
2020-05-13 01:43:19 +0500

    simplify reward weight generator

--- ppo_pytorch/algs/reward_weight_generator.py
+++ ppo_pytorch/algs/reward_weight_generator.py
@@ -7,14 +7,32 @@ class RewardWeightGenerator:
 
     @property
     def num_weights(self):
-        return 4 * self.num_rewards
+        return self.num_rewards
 
     def get_true_weights(self, reward_weights):
-        return reward_weights[..., :self.num_rewards]
+        return reward_weights#[..., :self.num_rewards]
 
     def generate(self, num_actors, device=None):
-        r = torch.rand((num_actors, self.num_rewards), device=device)
-        return torch.cat([r, -r, 1 - r, r - 1], -1)
+        # weights = torch.tensor([
+        #     (1, 0, 0),
+        #     (1, 0, 1),
+        #     (1, -0.2, 1),
+        #     (0, 1, 1),
+        #     (0, 0, 1),
+        #     (0, -0.2, 1),
+        #     (-0.2, 0, 1),
+        # ], device=device)
+        # idx = torch.randint(0, len(weights), size=(num_actors,), device=device)
+        # return weights[idx]
+
+        # r = torch.rand((num_actors, 1), device=device)
+        # return torch.cat([r * 0 + 1, r * 0, r * 0], -1)
+        # return torch.randn((num_actors, self.num_rewards), device=device) + 0.5
+        # return torch.cat([r, -r, 1 - r, r - 1], -1)
+
+        randn = torch.zeros((num_actors, self.num_rewards), device=device)
+        # randn[:, 0] += 1
+        return randn.softmax(dim=1)
 
 
 def test_RewardWeightGenerator():
