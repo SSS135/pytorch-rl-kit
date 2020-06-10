add gumbel softmax, fix impala dpg

ppo_pytorch/algs/impala.py
@@ -278,11 +278,13 @@ class IMPALA(RLBase):
     def _apply_pop_art(self):
         if self.use_pop_art:
             self._train_model.heads.state_values.normalize(*self._pop_art.statistics)
+            self._train_model.heads.action_values.normalize(*self._pop_art.statistics)
 
     def _unapply_pop_art(self, value_target_list: List[Tensor]):
         if self.use_pop_art:
             pa_mean, pa_std = self._pop_art.statistics
             self._train_model.heads.state_values.unnormalize(pa_mean, pa_std)
+            self._train_model.heads.action_values.unnormalize(pa_mean, pa_std)
             for vtarg in value_target_list:
                 self._pop_art.update_statistics(vtarg * pa_std + pa_mean)
             if self._do_log:
@@ -324,7 +326,7 @@ class IMPALA(RLBase):
 
             if do_log:
                 batch.state_values = log_gradients(batch.state_values, self.logger, 'Values', self.frame_train)
-                batch.action_advantages = log_gradients(batch.action_advantages, self.logger, 'Q Values', self.frame_train)
+                batch.action_values = log_gradients(batch.action_values, self.logger, 'Q Values', self.frame_train)
                 batch.dpg_values = log_gradients(batch.dpg_values, self.logger, 'Values Grad Act', self.frame_train)
                 batch.logits = log_gradients(batch.logits, self.logger, 'Logits', self.frame_train)
 
@@ -352,11 +354,13 @@ class IMPALA(RLBase):
             ac_out_target = self._target_model(states, goal=reward_weights, evaluate_heads=['logits'])
 
         state_values = self._train_model.heads.state_values(ac_out_train.features_0)
-        action_advantages = self._train_model.heads.action_values(ac_out_train.features_0, actions=actions_replay)
+        action_values = self._train_model.heads.action_values(ac_out_train.features_0, actions=actions_replay)
 
-        actions_desired = self._train_model.heads.logits.pd.sample(ac_out_train.logits)
-        dpg_values = self._calc_dpg(actions_desired, ac_out_train.features_0) \
-            if self.dpg_loss_scale != 0 else state_values.detach()
+        if self.dpg_loss_scale != 0:
+            actions_desired = self._train_model.heads.logits.pd.rsample(ac_out_train.logits)
+            dpg_values = self._calc_dpg(actions_desired, ac_out_train.features_0)
+        else:
+            dpg_values = state_values.detach()
 
         assert state_values.shape == dpg_values.shape == (*states.shape[:2], 1), \
             (state_values.shape, dpg_values.shape, (*states.shape[:2], 1), states.shape)
@@ -365,7 +369,7 @@ class IMPALA(RLBase):
             logits=ac_out_train.logits,
             state_values=state_values.squeeze(-1),
             dpg_values=dpg_values.squeeze(-1),
-            action_advantages=action_advantages.squeeze(-1),
+            action_values=action_values.squeeze(-1),
             logits_target=ac_out_target.logits,
             **(dict(features_raw=ac_out_train.features_raw_0) if hasattr(ac_out_train, 'features_raw_0') else {}),
         )
@@ -386,18 +390,20 @@ class IMPALA(RLBase):
         # features_raw = torch.cat([actor_out_burn_in.features_raw_0, actor_out.features_raw_0], 0)
 
         state_values = self._train_model.heads.state_values(features)
-        action_advantages = self._train_model.heads.action_values(features, actions=actions_replay)
+        action_values = self._train_model.heads.action_values(features, actions=actions_replay)
 
-        actions_desired = self._train_model.heads.logits.pd.sample(logits)
-        dpg_values = self._calc_dpg(actions_desired, features) \
-            if self.dpg_loss_scale != 0 else state_values.detach()
+        if self.dpg_loss_scale != 0:
+            actions_desired = self._train_model.heads.logits.pd.rsample(logits)
+            dpg_values = self._calc_dpg(actions_desired, features)
+        else:
+            dpg_values = state_values.detach()
         assert state_values.shape == dpg_values.shape == (*states.shape[:-1], 1)
 
         return dict(
             logits=logits,
             state_values=state_values.squeeze(-1),
             dpg_values=dpg_values.squeeze(-1),
-            action_advantages=action_advantages.squeeze(-1),
+            action_values=action_values.squeeze(-1),
             logits_target=actor_out_target.logits,
             # features_raw=features_raw,
         )
@@ -410,18 +416,18 @@ class IMPALA(RLBase):
         for p in head.parameters():
             p.requires_grad = False
 
-        action_advantages = head(features.detach(), actions=actions)
+        action_values = head(features.detach(), actions=actions)
 
         for p in head.parameters():
             p.requires_grad = True
 
-        assert action_advantages.requires_grad
-        return action_advantages
+        assert action_values.requires_grad
+        return action_values
 
     def _get_impala_loss(self, data, do_log=False, tag=''):
-        state_values, action_advantages = data.state_values, data.action_advantages
+        state_values, action_values = data.state_values, data.action_values
         data.update({k: v[:-1] for k, v in data.items()})
-        data.state_values, data.action_advantages = state_values, action_advantages
+        data.state_values, data.action_values = state_values, action_values
 
         pd = self._train_model.heads.logits.pd
         data.logp_replay = pd.logp(data.actions, data.logits_replay)
@@ -435,7 +441,7 @@ class IMPALA(RLBase):
         with torch.no_grad():
             self._process_rewards(data, do_log)
         data.state_values = data.state_values[:-1]
-        data.action_advantages = data.action_advantages[:-1]
+        data.action_values = data.action_values[:-1]
 
         loss_world_model = self._get_world_model_loss(data, do_log)
 
@@ -452,7 +458,7 @@ class IMPALA(RLBase):
         loss_kl = self.kl_pull * data.kl_target.mean()
         loss_ent = self.entropy_loss_scale * -data.entropy.mean()
         loss_state_value = self.value_loss_scale * barron_loss(data.state_values, data.state_value_targets, *self.barron_alpha_c, reduce=False)
-        loss_action_advantage = self.q_loss_scale * barron_loss(data.action_advantages, data.action_advantage_targets, *self.barron_alpha_c, reduce=False)
+        loss_action_value = self.q_loss_scale * barron_loss(data.action_values, data.action_value_targets, *self.barron_alpha_c, reduce=False)
 
         # kl_mask = (data.kl_target.detach().mean(-1) <= self.kl_limit).float()
         # assert kl_mask.shape == data.dpg_values.shape
@@ -465,15 +471,15 @@ class IMPALA(RLBase):
             loss_act_norm = act_norm * max(self.activation_norm_scale, 1 - self.frame_train / 100_000)
 
         assert loss_state_value.shape == data.state_values.shape, (loss_state_value.shape, data.state_values.shape)
-        assert loss_action_advantage.shape == loss_dpg.shape == loss_state_value.shape
+        assert loss_action_value.shape == loss_dpg.shape == loss_state_value.shape
 
         loss_dpg = loss_dpg.mean()
         loss_ent = loss_ent.mean()
         loss_state_value = loss_state_value.mean()
-        loss_action_advantage = loss_action_advantage.mean()
+        loss_action_value = loss_action_value.mean()
 
         # sum all losses
-        total_loss = loss_pg + loss_kl + loss_state_value + loss_action_advantage + loss_dpg + loss_world_model + \
+        total_loss = loss_pg + loss_kl + loss_state_value + loss_action_value + loss_dpg + loss_world_model + \
                      loss_ent + loss_act_norm
         assert not np.isnan(total_loss.mean().item()) and not np.isinf(total_loss.mean().item()), \
             (loss_pg.mean().item(), loss_state_value.mean().item())
@@ -494,7 +500,7 @@ class IMPALA(RLBase):
                 self.logger.add_scalar('VTrace P/Above 0.25 Fraction', (data.vtrace_p > 0.25).float().mean(), self.frame_train)
                 self.logger.add_scalar('Stability/Entropy' + tag, data.entropy.mean(), self.frame_train)
                 self.logger.add_scalar('Losses/State Value' + tag, loss_state_value.mean(), self.frame_train)
-                self.logger.add_scalar('Losses/Q Value' + tag, loss_action_advantage.mean(), self.frame_train)
+                self.logger.add_scalar('Losses/Q Value' + tag, loss_action_value.mean(), self.frame_train)
                 if LossType.v_mpo is self.loss_type:
                     self.logger.add_scalar('Losses/KL Blend' + tag, loss_kl, self.frame_train)
                 self.logger.add_histogram('Losses/Value Hist' + tag, loss_state_value, self.frame_train)
@@ -560,15 +566,15 @@ class IMPALA(RLBase):
         state_value_targets, advantages, data.vtrace_p = calc_vtrace(
             data.rewards, state_values,
             data.dones, data.probs_ratio.detach().mean(-1), data.kl_replay.detach().mean(-1),
-            self.reward_discount, self.vtrace_max_ratio, self.vtrace_kl_limit)
+            self.reward_discount, self.vtrace_max_ratio, self.vtrace_kl_limit, gae_lambda=0.95)
 
-        action_values = data.action_advantages.detach() + state_values
+        action_values = data.action_values.detach() + state_values
         if self.use_pop_art:
             action_values = action_values * pa_std + pa_mean
         advantages_upgo = calc_upgo(data.rewards, state_values,
                                     data.dones, self.reward_discount,
-                                    gae_lambda=1.0, action_values=action_values if self.q_loss_scale > 0 else None) - state_values[:-1]
-        action_advantage_targets = data.rewards + self.reward_discount * (1 - data.dones) * state_value_targets[1:] - state_values[:-1]
+                                    gae_lambda=0.95, action_values=action_values if self.q_loss_scale > 0 else None) - state_values[:-1]
+        action_value_targets = data.rewards + self.reward_discount * (1 - data.dones) * state_value_targets[1:]
         state_value_targets = state_value_targets[:-1]
 
         if do_log:
@@ -576,8 +582,8 @@ class IMPALA(RLBase):
             self.logger.add_scalar('Advantages/RMS', advantages.pow(2).mean().sqrt(), self.frame_train)
             self.logger.add_scalar('Advantages/UPGO Mean', advantages_upgo.mean(), self.frame_train)
             self.logger.add_scalar('Advantages/UPGO RMS', advantages_upgo.pow(2).mean().sqrt(), self.frame_train)
-            self.logger.add_scalar('Advantages/Act Adv Mean', action_advantage_targets.mean(), self.frame_train)
-            self.logger.add_scalar('Advantages/Act Adv RMS', action_advantage_targets.pow(2).mean().sqrt(), self.frame_train)
+            self.logger.add_scalar('Advantages/Act Adv Mean', action_value_targets.mean(), self.frame_train)
+            self.logger.add_scalar('Advantages/Act Adv RMS', action_value_targets.pow(2).mean().sqrt(), self.frame_train)
             self.logger.add_scalar('Advantages/Std', advantages.std(), self.frame_train)
             self.logger.add_scalar('Values/Values', state_values.mean(), self.frame_train)
             self.logger.add_scalar('Values/Values RMS', state_values.pow(2).mean().sqrt(), self.frame_train)
@@ -585,7 +591,7 @@ class IMPALA(RLBase):
             self.logger.add_scalar('Values/Value Targets', state_value_targets.mean(), self.frame_train)
 
         if self.use_pop_art:
-            action_advantage_targets = action_advantage_targets / pa_std
+            action_value_targets = (action_value_targets - pa_mean) / pa_std
             state_value_targets = (state_value_targets - pa_mean) / pa_std
             if do_log:
                 self.logger.add_scalar('Values/Values PopArt', data.state_values.mean(), self.frame_train)
@@ -595,7 +601,7 @@ class IMPALA(RLBase):
         advantages = self._adv_norm(self.pg_loss_scale * advantages + self.upgo_scale * data.vtrace_p * advantages_upgo)
 
         data.state_value_targets, data.advantages = state_value_targets, advantages
-        data.action_advantage_targets = action_advantage_targets
+        data.action_value_targets = action_value_targets
 
     def drop_collected_steps(self):
         self._step_collector.drop_collected_steps()

ppo_pytorch/common/gae.py
@@ -120,7 +120,7 @@ def calc_upgo(rewards: torch.Tensor, values: torch.Tensor, dones: torch.Tensor,
 @torch.jit.script
 def calc_vtrace(rewards: torch.Tensor, values: torch.Tensor, dones: torch.Tensor,
                 probs_ratio: torch.Tensor, kl_div: torch.Tensor,
-                discount: float, max_ratio: float = 1.0, kl_limit: float = 0.3, lam: float = 1.0,
+                discount: float, max_ratio: float, kl_limit: float, gae_lambda: float,
                 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     _check_data(rewards, values, dones)
     assert probs_ratio.shape == rewards.shape == kl_div.shape == dones.shape, (probs_ratio.shape, rewards.shape, kl_div.shape)
@@ -128,7 +128,7 @@ def calc_vtrace(rewards: torch.Tensor, values: torch.Tensor, dones: torch.Tensor
 
     nonterminal = 1 - dones
     kl_mask = 1.0 - (kl_div / kl_limit).clamp_max(1.0)
-    c = p = (probs_ratio.clamp_max(max_ratio) * kl_mask).clamp_max(lam)
+    c = p = (probs_ratio.clamp_max(max_ratio) * kl_mask).clamp_max(gae_lambda)
     deltas = p * (rewards + nonterminal * discount * values[1:] - values[:-1])
     nonterm_c = nonterminal * c
     vs_minus_v_xs = torch.zeros_like(values)

ppo_pytorch/common/probability_distributions.py
@@ -11,22 +11,23 @@ import torch
 import torch.jit
 import torch.distributions
 import torch.nn.functional as F
-from torch.distributions import Beta, kl_divergence, Normal
+from torch.distributions import Beta, kl_divergence, Normal, Gumbel
 
 
 def make_pd(space: gym.Space):
     """Create `ProbabilityDistribution` from gym.Space"""
     if isinstance(space, gym.spaces.Discrete):
-        return CategoricalPd(space.n)
+        # return CategoricalPd(space.n)
+        return GumbelCategoricalPd(space.n)
     elif isinstance(space, gym.spaces.Box):
         assert len(space.shape) == 1
         # return LinearTanhPd(space.shape[0])
         # return FixedStdGaussianPd(space.shape[0], 1.0)
         # return BetaPd(space.shape[0], 1)
-        # return DiagGaussianPd(space.shape[0], max_norm=2.0)
+        return DiagGaussianPd(space.shape[0], max_norm=2.0)
         # return MixturePd(space.shape[0], 4, partial(BetaPd, h=1))
         # return PointCloudPd(space.shape[0], max_norm=2.0)
-        return DiscretizedCategoricalPd(space.shape[0], 7, ordinal=True)
+        # return DiscretizedCategoricalPd(space.shape[0], 7, ordinal=True)
     elif isinstance(space, gym.spaces.MultiBinary):
         return BernoulliPd(space.n)
     elif isinstance(space, gym.spaces.MultiDiscrete):
@@ -80,6 +81,10 @@ class ProbabilityDistribution:
 
     def sample(self, prob):
         """Sample action from probabilities"""
+        raise NotImplementedError()
+
+    def rsample(self, prob):
+        """Sample with gradient"""
         raise NotImplementedError()
 
     def to_inputs(self, action):
@@ -136,18 +141,43 @@ class CategoricalPd(ProbabilityDistribution):
         return -p_log_p.sum(-1, keepdim=True)
 
     def sample(self, logits):
-        logits = self._process_logits(logits)
-        return F.softmax(logits.reshape(-1, logits.shape[-1]), dim=-1).multinomial(1).reshape(*logits.shape[:-1], -1)
+        with torch.no_grad():
+            logits = self._process_logits(logits)
+            return F.softmax(logits.reshape(-1, logits.shape[-1]), dim=-1)\
+                .multinomial(1).reshape(*logits.shape[:-1], -1)
 
     def to_inputs(self, action):
         with torch.no_grad():
             onehot = torch.zeros((*action.shape[:-1], self.n), device=action.device)
             onehot.scatter_(dim=-1, index=action, value=1)
-        return onehot
+            return onehot
 
     def _process_logits(self, logits):
         assert logits.shape[-1] == self.n, (logits.shape[-1], self.n)
         return make_logits_ordnial(logits) if self.ordinal else logits
+
+
+class GumbelCategoricalPd(CategoricalPd):
+    def __init__(self, n, ordinal=False, temp=1):
+        super().__init__(n, ordinal)
+        self.temp = temp
+        self.gumbel = Gumbel(0, 1)
+
+    def sample(self, logits):
+        with torch.no_grad():
+            softmax = self.rsample(logits)
+            return softmax.max(-1)[1].unsqueeze(-1)
+
+    def rsample(self, logits):
+        logits = self._process_logits(logits)
+        logits = (logits + self.gumbel.sample(logits.shape).to(logits.device)) / self.temp
+        return F.softmax(logits, dim=-1)
+
+    def to_inputs(self, action):
+        if action.is_floating_point():
+            assert action.shape[-1] == self.n
+            return action
+        return super().to_inputs(action)
 
 
 class MultiPd(ProbabilityDistribution):
@@ -193,8 +223,14 @@ class MultiPd(ProbabilityDistribution):
         return torch.cat(all_ent, -1)
 
     def sample(self, all_logits):
+        with torch.no_grad():
+            split_logits = all_logits.split(self._prob_sizes, -1)
+            all_actions = [pd.sample(logits).type(self.dtype) for pd, logits in zip(self.pds, split_logits)]
+            return torch.cat(all_actions, -1)
+
+    def rsample(self, all_logits):
         split_logits = all_logits.split(self._prob_sizes, -1)
-        all_actions = [pd.sample(logits).type(self.dtype) for pd, logits in zip(self.pds, split_logits)]
+        all_actions = [pd.rsample(logits).type(self.dtype) for pd, logits in zip(self.pds, split_logits)]
         return torch.cat(all_actions, -1)
 
     def to_inputs(self, all_actions):
@@ -303,6 +339,10 @@ class DiagGaussianPd(ProbabilityDistribution):
         )
 
     def sample(self, prob):
+        with torch.no_grad():
+            return self.rsample(prob)
+
+    def rsample(self, prob):
         mean, logstd = self._split_probs(prob)
         std = logstd.exp()
         return mean + std * torch.randn_like(mean)
@@ -365,6 +405,10 @@ class PointCloudPd(ProbabilityDistribution):
         )
 
     def sample(self, prob):
+        with torch.no_grad():
+            return self.rsample(prob)
+
+    def rsample(self, prob):
         prob = self._split_probs(prob)
         idx = torch.randint(0, self.cloud_size, prob.shape[:-2], device=prob.device)
         idx = torch.zeros((*prob.shape[:-1], 1), dtype=idx.dtype, device=prob.device) + idx.unsqueeze(-1).unsqueeze(-1)
@@ -474,6 +518,10 @@ class FixedStdGaussianPd(ProbabilityDistribution):
         return torch.zeros_like(mean) + (0.5 + 0.5 * math.log(2 * math.pi) + math.log(self.std))
 
     def sample(self, mean):
+        with torch.no_grad():
+            return self.rsample(mean)
+
+    def rsample(self, mean):
         return self._clamp_logits(mean + self.std * torch.randn_like(mean))
 
     def _clamp_logits(self, logits):

ppo_pytorch/examples/atari_impala.py
@@ -1,4 +1,5 @@
 from optfn.gadam import GAdam
+from torch.optim import AdamW
 
 if __name__ == '__main__':
     from .init_vars import *
@@ -35,21 +36,21 @@ if __name__ == '__main__':
         use_pop_art=True,
         reward_scale=1.0,
         kl_pull=0.5,
-        eval_model_blend=0.1,
+        eval_model_blend=0.5,
         vtrace_max_ratio=1.0,
-        vtrace_kl_limit=0.5,
-        kl_limit=0.3,
+        vtrace_kl_limit=0.3,
+        kl_limit=1.0,
         loss_type='impala',
         replay_ratio=3,
-        upgo_scale=0.2,
+        upgo_scale=0.0,
         entropy_loss_scale=0.01,
         barron_alpha_c=(2.0, 1.0),
         memory_burn_in_steps=32,
-        activation_norm_scale=0.003,
+        activation_norm_scale=0.0,
         reward_reweight_interval=40,
         random_crop_obs=False,
 
-        optimizer_factory=partial(optim.AdamW, lr=3e-4),
+        optimizer_factory=partial(GAdam, lr=5e-4, avg_sq_mode='tensor', betas=(0.9, 0.99)),
         model_factory=partial(create_impala_cnn_actor, cnn_kind='normal'),
         # model_factory=partial(create_impala_rnn_actor, hidden_size=256, num_layers=3),
         # model_factory=partial(create_impala_attention_actor, num_units=9, unit_size=7,
@@ -58,7 +59,7 @@ if __name__ == '__main__':
         #                       activation=SiLU, use_imagination=False),
     )
     trainer_params = dict(
-        tag='[ent0.01_r3_upgo0.2_an0.003_ort_pa_kllim0.3_vtkllim0.5_blend0.1_klpull0.5_rclip_oldheadinit_h64]',
+        tag='[tavgsq_ent0.01_r3_upgo0_an0_ort_pa_kllim0.3_vtkllim0.3_blend0.5_klpull0.5_rclip_oldheadinit_h64]',
         log_root_path=log_path,
         log_interval=20000,
         rl_alg_factory=partial(alg_class, **alg_params),