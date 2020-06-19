b6c53c3d4c9b270080aec05163ad855da17d6f81
Alexander Penkin <sss13594@gmail.com>
2020-06-19 12:11:19 +0500

    remove use_rms from PopArt

--- ppo_pytorch/common/pop_art.py
+++ ppo_pytorch/common/pop_art.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import torch
 
 
@@ -8,12 +6,11 @@ def clip(x, x_min, x_max):
 
 
 class PopArt:
-    def __init__(self, beta=0.01, use_rms=False, eps=(1e-2, 1e6)):
+    def __init__(self, beta=0.01, eps=(1e-2, 1e6)):
         assert 0 < beta < 1
         assert 0 <= eps[0] < eps[1]
         self.beta = beta
         self.eps = eps
-        self.use_rms = use_rms
         self._mean = 0
         self._square = 0
         self._update_iteration = 0
@@ -26,12 +23,8 @@ class PopArt:
             bias_corr = 1 - (1 - self.beta) ** self._update_iteration
             square = self._square / bias_corr
             mean = self._mean / bias_corr
-            if self.use_rms:
-                rms = clip(square ** 0.5, *self.eps)
-                return 0.0, rms
-            else:
-                std = clip((square - mean ** 2) ** 0.5, *self.eps)
-                return mean, std
+            std = clip((square - mean ** 2) ** 0.5, *self.eps)
+            return mean, std
 
     def update_statistics(self, returns: torch.Tensor):
         self._update_iteration += 1
